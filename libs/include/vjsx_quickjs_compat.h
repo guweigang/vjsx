d09c459969800a39414774b6f27ab8cdc8af5727
weigang <guweigang@bullsoft.org>
2026-05-10 16:27:59 +0800

    fix: map strict equality for quickjs-ng

--- libs/include/vjsx_quickjs_compat.h
+++ libs/include/vjsx_quickjs_compat.h
@@ -6,6 +6,7 @@
 #if defined(VJSX_QUICKJS_NG)
 #define JS_IsArray(ctx, val) JS_IsArray(val)
 #define JS_IsError(ctx, val) JS_IsError(val)
+#define JS_StrictEq(ctx, op1, op2) JS_IsStrictEqual(ctx, op1, op2)
 #endif
 
 /*
